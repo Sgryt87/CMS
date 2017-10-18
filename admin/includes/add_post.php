b8e0722469fa934488a063006c83ae3481ea825d
Sergii <sergiigrytsaienko@gmail.com>
2017-10-18 00:39:13 +0000

    post form done

--- admin/includes/add_post.php
+++ admin/includes/add_post.php
@@ -1,7 +1,21 @@
 <?php
 
 if (isset($_POST['create_post'])) {
-    echo $_POST['create_post'];
+
+    $post_title = $_POST['title'];
+    $post_author = $_POST['author'];
+    $post_category_id = $_POST['post_category_id'];
+    $post_status = $_POST['post_status'];
+
+    $post_image = $_FILES['image']['name'];
+    $post_image_temp = $_FILES['image']['tmp_name'];
+
+    $post_tags = $_POST['post_tags'];
+    $post_content = $_POST['post_content'];
+    $post_date = date('d-m-y');
+    $post_comment_count = 4;
+
+    move_uploaded_file($post_image_temp, "../images/$post_image");
 }
 ?>
 
